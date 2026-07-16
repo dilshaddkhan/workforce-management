41acfe8639a5c4800cbf707b7ee3c89b32aff63f
dilshaddkhan <khanmohammaddilshad@gmail.com>
2026-07-16 17:38:12 +0530

    bypssing the actuator jwt authetication

--- src/main/java/com/company/workforce/config/SecurityConfig.java
+++ src/main/java/com/company/workforce/config/SecurityConfig.java
@@ -76,7 +76,8 @@ public class SecurityConfig {
                                         "/v3/api-docs/**",
                                         "/swagger-ui.html",
                                         "/api/v1/auth/register",
-                                        "/api/v1/auth/login"
+                                        "/api/v1/auth/login",
+                                        "/actuator/**"
 
                                 )
                                 .permitAll()
