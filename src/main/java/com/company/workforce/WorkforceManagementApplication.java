eded820e291d2aecd362be213b628b1eb6ef2544
dilshaddkhan <khanmohammaddilshad@gmail.com>
2026-07-16 19:59:23 +0530

    configuring the actuator

--- src/main/java/com/company/workforce/WorkforceManagementApplication.java
+++ src/main/java/com/company/workforce/WorkforceManagementApplication.java
@@ -3,6 +3,7 @@ package com.company.workforce;
 import com.company.workforce.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
@@ -12,11 +13,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class WorkforceManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(
-                WorkforceManagementApplication.class,
-                args
-        );
+        SpringApplication app = new SpringApplication(WorkforceManagementApplication.class);
+        app.setApplicationStartup(new BufferingApplicationStartup(2048));
+        app.run(args);
     }
-
 
 }
